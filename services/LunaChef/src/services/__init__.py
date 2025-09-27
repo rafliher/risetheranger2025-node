bee17d7d807232fdb870ef4e9a3d509141c977d2
wondping0 <faturrohmansugiyarto@gmail.com>
2025-09-28 02:03:24 +0700

    chech commit: lunachef

--- services/LunaChef/src/services/__init__.py
+++ services/LunaChef/src/services/__init__.py
@@ -2,7 +2,14 @@
 Services Package
 Contains individual service files for each cryptographic operation
 """
+from config import key
+from config import hash_key
+from config import signing_key
 
-from .service_encryption import encryption_service
-from .service_signing import signing_service
-from .service_hash import hashing_service
+from .service_encryption import EncryptionService
+from .service_signing import SigningService
+from .service_hash import HashingService
+
+encryption_service = EncryptionService(key)
+signing_service = SigningService(4, signing_key)
+hashing_service = HashingService(hash_key)
